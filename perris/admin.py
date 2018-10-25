d41eac589cdd6ad418b04f3c45d15862240d12da
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2018-10-25 18:56:17 -0300

    Mis perris, primer commit.. de nuevo

---
+++ perris/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from perris.models import Persona, Perro
+
+# Register your models here.
+admin.site.register(Persona)
+admin.site.register(Perro)
---
+++ perris/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=70)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=12)
+    email = models.EmailField()
+    direccion = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Perro(models.Model):
+    estado_opc = (
+        ('AD', 'Adoptado'),
+        ('RE', 'Rescatado'),
+        ('DI', 'Disponible'),
+    )
+    nombre = models.CharField(max_length=50)
+    raza = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=2, choices=estado_opc)
+    foto = models.ImageField(upload_to='media/rescatados/')
+    persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
