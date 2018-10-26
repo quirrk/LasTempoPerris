5411fbe20b2d60a16851bb0218b00687babe987d
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2018-10-25 23:06:19 -0300

    Se añaden archivos static

--- misperris/settings.py
+++ misperris/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'misperris.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
