b0ffe7d30abf968ffc175ea02262f3da312dd1b3
Felipe Herrera Tapia <fe.herrerat@alumnos.duoc.cl>
2018-10-29 19:03:15 -0300

    se agregan url pattern intento solucion

--- misperris/urls.py
+++ misperris/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include ('perris.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
