bc3c05528cb490e3d24729d02455060f7bc0eef4
Felipe Herrera Tapia <fe.herrerat@alumnos.duoc.cl>
2018-10-28 22:09:07 -0300

    se resuelven conflictos, settings.py urls.py

--- misperris/settings.py
+++ misperris/settings.py
@@ -27,9 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    ' . pythonanywhere.com'
-    'criterion1.pythonanywhere.com'
-    'quirrk.pythonanywhere.com'
+    ' . pythonanywhere.com',
+    'criterion1.pythonanywhere.com',
 ]
 
 
