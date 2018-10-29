32263e2fccf3ecac6f1c0ca4638f04aab7aa9811
Felipe Herrera Tapia <fe.herrerat@alumnos.duoc.cl>
2018-10-29 20:23:22 -0300

    se termina log in/out pass reset sin css

--- misperris/settings.py
+++ misperris/settings.py
@@ -130,3 +130,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
