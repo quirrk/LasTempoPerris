1a5e422808c147660305874bf7bfb9b7e5aab8f7
Felipe Herrera <felipe.ah14@gmail.com>
2019-05-20 19:09:22 -0400

    la wea

--- perris/models.py
+++ perris/models.py
@@ -13,16 +13,3 @@ class Persona(models.Model):
     def __str__(self):
         return self.nombre
 
-class Perro(models.Model):
-    estado_opc = (
-        ('AD', 'Adoptado'),
-        ('RE', 'Rescatado'),
-        ('DI', 'Disponible'),
-    )
-    nombre = models.CharField(max_length=50)
-    raza = models.CharField(max_length=50)
-    descripcion = models.TextField()
-    estado = models.CharField(max_length=2, choices=estado_opc)
-    foto = models.ImageField(upload_to='media/rescatados/')
-    def __str__(self):
-        return self.nombre
